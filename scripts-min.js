7155f50ceec7985fa7c4d5441d56588116e318cb
Joshua Hua Shao <jshao@joshuas-mbp.lan>
2018-03-14 08:37:02 -0400

    final commit

--- scripts-min.js
+++ scripts-min.js
@@ -1,1 +1,1 @@
-console.log("hello world!");
+console.log("hello world!"),$(".bodytext").removeClass("hide"),$(".bodytext").addClass("animated fadeInRight"),$(".title").hover(function(){$(".title").removeClass("animated bounceIn"),$(".title").addClass("animated hinge")}),$(window).scroll(function(){$(".title").removeClass("animated hinge"),$(".title").addClass("animated bounceIn")}),/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)&&($(".title").addClass("animated hinge"),$(window).scroll(function(){$(".title").addClass("animated bounceIn")}));
